0973cb4d52c9fc7635b6f424ed46b5326570784e
Vadim Kaluga <kalugavadim04@gmail.com>
2024-03-15 14:06:52 +0700

    add teacher struct

---
+++ teacher.c
@@ -0,0 +1,13 @@
+#ifndef __TEACHER
+#define __TEACHER
+
+/* TODO: связать преподавателя с его специальностью */
+struct teacher
+{
+    int id;
+    char *name;
+    char *surname;
+    char *midname; /* отчество */
+};
+
+#endif /* __TEACHER */
