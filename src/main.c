477c916c4623938ab16687d308abde4a163215d1
Vadim Kaluga <kalugavadim04@gmail.com>
2024-03-15 17:24:16 +0700

    department struct

--- src/main.c
+++ src/main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 
 #include "../headers/teacher.h"
+#include "../headers/schedule.h"
 
 int main(int argc, char **argv)
 {
     struct teacher *pavel = new_teacher(100, "Павел", "Ананьев", "Иванович");
+    printf("[debug] id = %d: %s %s %s", pavel->id, pavel->surname, pavel->name, pavel->midname);
 
-    printf("%d: %s %s %s", pavel->id, pavel->surname, pavel->name, pavel->midname);
+    /*
+#define GEN_IN_ONE_CHROMOSOME 2 
+    struct gene *gene = malloc(sizeof(struct gene) * GEN_IN_ONE_CHROMOSOME);
+    */
 
     return 0;
 }
