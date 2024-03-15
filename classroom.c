f046f749a7e9d778036001a98bf27e6d8b9509a3
Vadim Kaluga <kalugavadim04@gmail.com>
2024-03-15 13:18:52 +0700

    classroom added

---
+++ classroom.c
@@ -0,0 +1,19 @@
+#ifndef __CLASSROOM
+#define __CLASSROOM
+
+enum rtype {
+    BIG_LECTURE,    /* более 3-х групп */
+    MEDIUM_LECTURE, /* 3 группы */
+    SMALL_LECTURE,  /* мньше 3-х групп */
+    PRACTICAL,
+    LABORATORY
+};
+
+struct room {
+    int room_number;
+    int building_number; /* номер корпуса */
+    int capacity;
+    enum rtype type;
+};
+
+#endif /* __CLASSROOM */
